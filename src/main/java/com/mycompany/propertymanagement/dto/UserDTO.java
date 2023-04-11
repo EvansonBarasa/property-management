39e19d7152a8cb7eef6e42d42c2733a7e406a5f8
Evanson Barasa <evanzo85@gmail.com>
2023-04-11 16:42:14 +0300

    Implemented logging

--- src/main/java/com/mycompany/propertymanagement/dto/UserDTO.java
+++ src/main/java/com/mycompany/propertymanagement/dto/UserDTO.java
@@ -6,6 +6,9 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.Column;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Getter
 @Setter
@@ -14,7 +17,12 @@ import javax.persistence.Column;
 public class UserDTO {
     private Long id;
     private String ownerName;
+    @NotNull(message = "Email address should be null/mandatory")
+    @Size(min = 1, max = 50, message = "Owner email should be between 1 and 50")
+    @NotEmpty(message = "Owner Email cannot be empty")
     private String ownerEmail;
     private String phone;
+    @NotNull(message = "Password cannot be null")
+    @NotEmpty(message = "Password cannot be empty")
     private String password;
 }
